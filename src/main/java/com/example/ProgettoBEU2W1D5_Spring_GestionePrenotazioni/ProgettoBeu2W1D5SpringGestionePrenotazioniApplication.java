4920613615ebbd37112b7f8b8a3820d59b19859f
Edoardo <edfl.tunzi@gmail.com>
2025-02-07 11:40:57 +0100

    initial commit

---
+++ src/main/java/com/example/ProgettoBEU2W1D5_Spring_GestionePrenotazioni/ProgettoBeu2W1D5SpringGestionePrenotazioniApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProgettoBEU2W1D5_Spring_GestionePrenotazioni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoBeu2W1D5SpringGestionePrenotazioniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoBeu2W1D5SpringGestionePrenotazioniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProgettoBEU2W1D5_Spring_GestionePrenotazioni/ProgettoBeu2W1D5SpringGestionePrenotazioniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProgettoBEU2W1D5_Spring_GestionePrenotazioni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoBeu2W1D5SpringGestionePrenotazioniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
